57a3743d0704d7dce4bde90de0042cf67db4a7b7
Natan Utevsky <99504834+nathutev@users.noreply.github.com>
2023-08-09 11:54:08 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -11,23 +11,13 @@ function update(){
   function formatTime(time){
     let hour = time.getHours();
     let minute = time.getMinutes();
-    let second = time.getSeconds();
-
 
     if(hour <= 9 && minute <= 9){
       return `0${hour}:0${minute}`;
     }
 
-    else if(hour <= 9 && minute <= 9){
-      return `0${hour}:0${minute}`;
-    }
-
-    else if(minute <= 9){
-      return `${hour}:0${minute}`;
-    }
-
     else if(hour <= 9){
-      return `${hour}:0${minute}`;
+      return `0${hour}:${minute}`;
     }
 
     else if(minute <= 9){
